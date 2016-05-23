dcc7208afc8063e04a2068a51eeef76015bb7f9e
87maza <87maza@gmail.com>
2016-05-22 22:55:54 -0700

    added config and public folder with api routes

--- server.js
+++ server.js
@@ -4,29 +4,25 @@ var fs = require('fs');
 var favicon = require('serve-favicon');
 var port = process.env.PORT || 3000
 var app = express();
+var mongoose = require('mongoose');
 app.use(favicon(__dirname + '/android-icon-192x192.png'));
 
 app.use(morgan('short'));
 
-app.get('/', function(req,res){
-	fs.createReadStream(__dirname + '/index.html').pipe(res);
-})
+//home route
+app.use(express.static('public'));
 
 
-app.get('/data', function(req,res){
-	var ip = req.headers['x-forwarded-for'] || 
-    	req.connection.remoteAddress || 
-    	req.socket.remoteAddress ||
-    	req.connection.socket.remoteAddress;
-    var lang = req.headers['accept-language'].substring(0,5);
-    var os = req.headers['user-agent'].match(/\((.+?)\)/,"")[1];
-    var obj = {
-    	ip : ip,
-    	language: lang,
-    	os: os
-    }
-    res.json(obj);
+mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name);
+
+app.get('/api/results', function(req,res){
+    res.send('search results page');
 })
+
+app.get('/api/history', function(req,res){
+    res.send('history page');
+})
+
 
 
 app.listen(port, function(){
