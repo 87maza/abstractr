f15aafaf64b0afe000f4d233be48d5a31d499fe5
87maza <87maza@gmail.com>
2016-05-24 23:11:36 -0700

    iterates through results object

--- server.js
+++ server.js
@@ -9,42 +9,36 @@ var bodyParser = require('body-parser');
 var config = require('./config.js');
 var Bing = require('node-bing-api')({ accKey: "HUbThdlOtDm9tcc33MaX9Jei8H8Vv7TU2FurPoCwHXI" });
 
-// bing translations
-// body.d.results[1].MediaUrl = url
-// body.d.results[1].title = alt tag/snippet
-// body.d.results[1].Thumbnail.MediaUrl = thumbnail
-// body.d.results[1].SourceUrl = context
-
-
-    Bing.images("Ninja Turtles", {skip: 20, top: 10}, function(error, res, body){
-      results = {
-        url : body.d.results[2].MediaUrl,
-        snippet: body.d.results[2].Title,
-        thumbnail: body.d.results[2].Thumbnail.MediaUrl,
-        context: body.d.results[2].SourceUrl
-      } 
-      function getit(y){
-    app.get('/api/results', function(req,res){
-    res.json(results);
-
-    })
-    }
-    getit(results);
-
-    });
-
-
-
-
 app.use(favicon(__dirname + '/android-icon-192x192.png'));
 
 app.use(morgan('short'));
 
-//home route
+//home route assets inside public folder
 app.use(express.static('public'));
 
 
 mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name);
+
+
+Bing.images("Ninja Turtles", {skip: 20, top: 10}, function(error, res, body){
+  var results = {};
+    for(var i=0; i<body.d.results.length; i++){
+        results["result" + (i+1)] = {
+            url : body.d.results[i].MediaUrl,
+            snippet: body.d.results[i].Title,
+            thumbnail: body.d.results[i].Thumbnail.MediaUrl,
+            context: body.d.results[i].SourceUrl
+        }
+    }
+  function getit(y){
+    app.get('/api/results', function(req,res){
+    res.json(y);
+    })
+  }
+  getit(results);
+
+});
+
 
 app.get('/api/history', function(req,res){
 
