c48f0e4112d41466eb51c88500eeb8388663bc38
87maza <87maza@gmail.com>
2016-05-25 17:16:32 -0700

    results loading only on console, logs search term on button click

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var bodyParser = require('body-parser');
 var config = require('./config.js');
 var Bing = require('node-bing-api')({ accKey: "HUbThdlOtDm9tcc33MaX9Jei8H8Vv7TU2FurPoCwHXI" });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(favicon(__dirname + '/android-icon-192x192.png'));
 
 app.use(morgan('short'));
@@ -19,32 +21,34 @@ app.use(express.static('public'));
 
 mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name);
 
-
-Bing.images("Ninja Turtles", {skip: 20, top: 10}, function(error, res, body){
-  var results = {};
-    for(var i=0; i<body.d.results.length; i++){
-        results["result" + (i+1)] = {
-            url : body.d.results[i].MediaUrl,
-            snippet: body.d.results[i].Title,
-            thumbnail: body.d.results[i].Thumbnail.MediaUrl,
-            context: body.d.results[i].SourceUrl
+app.post('/api/results', function(req,res){
+    Bing.images(req.body.searchTerm, {skip: 20, top: 10}, function(error, res, body){
+      var results = {};
+        for(var i=0; i<body.d.results.length; i++){
+            results["result" + (i+1)] = {
+                url : body.d.results[i].MediaUrl,
+                snippet: body.d.results[i].Title,
+                thumbnail: body.d.results[i].Thumbnail.MediaUrl,
+                context: body.d.results[i].SourceUrl
+            }
         }
-    }
-  function getit(y){
-    app.get('/api/results', function(req,res){
-    res.json(y);
-    })
-  }
-  getit(results);
-
-});
-
+      function getit(y){
+        console.log(y)
+        app.get('/api/results', function(req,res){
+        res.json(y);
+        })
+      }
+      getit(results);
+    });
+})
 
 app.get('/api/history', function(req,res){
 
 res.send('fuck')
     
 })
+
+
 
 
 
